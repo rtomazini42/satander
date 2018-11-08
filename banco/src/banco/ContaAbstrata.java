023b8c0118a57a618ec2e09414d9b9cad98d1e89
Renan Tomazini <renantomazini@gmail.com>
2018-11-08 19:11:05 -0300

    Create ContaAbstrata.java

---
+++ banco/src/banco/ContaAbstrata.java
@@ -0,0 +1,35 @@
+package banco;
+
+public abstract class ContaAbstrata {
+	private double saldo;
+	private int numero;
+	private String nome;
+	
+	public void creditar(double valor) {
+		this.saldo = this.saldo + valor;
+		
+	}
+	public abstract void debitar(double valor);
+	
+	public void renderJuros(double juros) {
+		this.saldo = this.saldo * juros;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
