50cab71a823ea5ee4a497ecdc2a07f97f3021e01
RENAN.TOMAZINI <RENAN.TOMAZINI@LAB-DEINFO-02.deinfo.lab33>
2018-11-08 17:49:49 -0300

    Objeto Conta adicionado

---
+++ banco/src/banco/Conta.java
@@ -0,0 +1,51 @@
+package banco;
+
+public class Conta {
+		private double saldo;
+		private int numero;
+		private String nome;
+		private int agencia;
+		
+		public Conta() {
+			this.saldo = 0.0;
+			this.nome = "User";
+			this.agencia = 123;
+			this.numero = 00000;
+		}
+
+		public double getSaldo() {
+			return saldo;
+		}
+
+		public void setSaldo(double saldo) {
+			this.saldo = saldo;
+		}
+
+		public int getNumero() {
+			return numero;
+		}
+
+		public void setNumero(int numero) {
+			this.numero = numero;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public int getAgencia() {
+			return agencia;
+		}
+
+		public void setAgencia(int agencia) {
+			this.agencia = agencia;
+		}
+		
+		
+	
+
+}
