f3a9b5d236ccbaec20b6f52e5cc6f37d7643d321
Renan Tomazini <renantomazini@gmail.com>
2018-11-08 19:11:32 -0300

    Update main.java

--- banco/src/banco/main.java
+++ banco/src/banco/main.java
@@ -8,6 +8,10 @@ public class main {
 		System.out.println(conta1.getSaldo());
 		conta1.creditar(20.30);
 		System.out.println(conta1.getSaldo());
+		conta1.debitar(5.10);
+		System.out.println(conta1.getSaldo());
+		conta1.renderJuros(1.07);
+		System.out.println(conta1.getSaldo());
 	}
 
 }
